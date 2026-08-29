a438e30c8c0ebacde61e6b2cf68f4be81ce5860c
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 06:47:44 +0200

    fix: remove duplicate global model controls mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { ChatModelControls } from './components/ChatModelControls';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <ChatModelControls />
   </StrictMode>,
 );
